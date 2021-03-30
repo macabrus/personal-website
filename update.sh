d453b7d27e5c32bd677d98570776f9f3fb109c5e
bernard.crnkovic <bernard.crnkovic@fer.hr>
2021-03-30 15:59:52 +0200

    final fix of uodate.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
-PID=$(cat pid.txt)
-kill -0 "$PID"
-wait "$PID"
+APP_PID=$(cat pid.txt)
+kill -0 "$APP_PID"
+wait "$APP_PID"
 git reset --hard HEAD
 eval `ssh-agent -s`
 ssh-add ~/.ssh/gitlab-bot
 git pull
 npm i
-nohup node app.js >logfile.txt 2>&1 & echo $! > pid.txt
+nohup node app.js > logfile.txt 2>&1 &
+echo $! > pid.txt
